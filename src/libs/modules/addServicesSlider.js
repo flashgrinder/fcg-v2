a19f8d22f00c17e29ab79568304f7c3bb6cd801f
Lelouch <flashgrinder92@gmail.com>
2021-09-02 17:52:35 +0400

    make add-services-slider

---
+++ src/libs/modules/addServicesSlider.js
@@ -0,0 +1,32 @@
+import Swiper, { Navigation } from 'swiper';
+
+function init() {
+
+    Swiper.use([Navigation]);
+
+    const addServicesSlider = new Swiper(".add-services-slider__swiper", {
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            nextEl: '.add-services-slider__arrow--next',
+            prevEl: '.add-services-slider__arrow--prev'
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 25
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 28,
+                slidesPerColumn: 1
+            }
+        }
+    });
+}
+
+export default { init }
--- src/libs/scripts-v2.js
+++ src/libs/scripts-v2.js
@@ -1,7 +1,9 @@
 import scrollContent from '../libs/modules/scrollContent.js';
+import addServicesSlider from '../libs/modules/addServicesSlider.js';
 
 document.addEventListener('DOMContentLoaded', function(e) {
 
     scrollContent.init();
+    addServicesSlider.init();
     
 });
