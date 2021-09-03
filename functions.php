65b333247cd0c1577760ded76d5c1d2a47056e7b
flashgrinder <flashgrinder92@gmail.com>
2021-09-03 13:47:27 +0400

    wip wp

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+    add_action('wp_enqueue_scripts', function() {
+
+        wp_enqueue_style('style-v2', get_stylesheet_directory_uri() . '/assets/css/style-v2.min.css', ['new-styles','styles-ver-2.0'], '2.0.0');
+
+        wp_enqueue_script('scripts-v2', get_stylesheet_directory_uri() . '/assets/js/scripts-v2.min.js', [], '2.0.0', true );
+
+    });
