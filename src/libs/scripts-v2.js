e4b2d3ae64904c521f6cf67d4b274fb68549e642
flashgrinder <flashgrinder92@gmail.com>
2021-09-29 15:32:09 +0400

    make tabs/wip services-calculator

--- src/libs/scripts-v2.js
+++ src/libs/scripts-v2.js
@@ -2,6 +2,7 @@ import scrollContent from '../libs/modules/scrollContent.js';
 import addServicesSlider from '../libs/modules/addServicesSlider.js';
 import customRange from '../libs/modules/customRange.js';
 import customSelect from '../libs/modules/customSelect.js';
+import tabs from '../libs/modules/tabs.js';
 
 document.addEventListener('DOMContentLoaded', function(e) {
 
@@ -9,10 +10,12 @@ document.addEventListener('DOMContentLoaded', function(e) {
     const scrollContentTrue = document.querySelector('.header-page__icon');
     const customRangeTrue = document.querySelector('.custom-range');
     const customSelectTrue = document.querySelector('.custom-select');
+    const tabsTrue = document.querySelector('.tab');
 
     addServicesSliderTrue ? addServicesSlider.init() : false;
     scrollContentTrue ? scrollContent.init() : false;
     customRangeTrue ? customRange.init() : false;
     customSelectTrue ? customSelect.init() : false;
+    tabsTrue ? tabs.init() : false;
     
 });
