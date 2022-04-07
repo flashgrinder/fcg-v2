63b9e7b0a3944ba801328895363cbba161c1bb17
flashgrinder <flashgrinder92@gmail.com>
2022-04-07 14:20:32 +0400

    test send form to bitrix24

--- inc/bitrix24-webhook.php
+++ inc/bitrix24-webhook.php
@@ -13,9 +13,9 @@
             $posted_data = $submission->get_posted_data();
 
             // Перехватывает введенные данные в Contact Form 7
-            $name = $posted_data['text-289'];
-            $email = $posted_data['email-289'];
-            $phone = $posted_data['tel-289'];
+            $name_289 = $posted_data['name-289'];
+            $email_289 = $posted_data['email-289'];
+            $phone_289 = $posted_data['phone-289'];
             $title = $posted_data['text-564'];
         
             $queryUrl = 'https://fincom.bitrix24.ru/rest/114/keviule2jvconggu/crm.lead.add.json';
@@ -23,16 +23,66 @@
             $queryData = http_build_query(array(
                 'fields' => array(
                     'TITLE' => 'Лид со страницы ' . $title,
-                    'NAME' => $name,
+                    'NAME' => $name_289,
                     'PHONE' => Array(
                         "n0" => Array(
-                            "VALUE" => $phone,
+                            "VALUE" => $phone_289,
                             "VALUE_TYPE" => "WORK"
                         )
                     ),
                     'EMAIL' => Array(
                         "n0" => Array(
-                            "VALUE" => $email,
+                            "VALUE" => $email_289,
+                            "VALUE_TYPE" => "WORK"
+                        )
+                    )
+                ),
+                'params' => array("REGISTER_SONET_EVENT" => "Y")
+            ));
+            
+            // обращаемся к Битрикс24 при помощи функции curl_exec
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_SSL_VERIFYPEER => 0,
+                CURLOPT_POST => 1,
+                CURLOPT_HEADER => 0,
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_URL => $queryUrl,
+                CURLOPT_POSTFIELDS => $queryData,
+            ));
+
+            $result = curl_exec($curl);
+            curl_close($curl);
+            $result = json_decode($result, 1);
+            if (array_key_exists('error', $result)) echo "Ошибка при сохранении лида: ".$result['error_description']."<br/>";
+        }
+
+        elseif ('Контактная форма(RU)v2' == $title ) {
+
+            $submission = WPCF7_Submission::get_instance();
+            $posted_data = $submission->get_posted_data();
+
+            // Перехватывает введенные данные в Contact Form 7
+            $name_861 = $posted_data['text-861'];
+            $email_141 = $posted_data['email-141'];
+            $phone_831 = $posted_data['tel-831'];
+            $title = $posted_data['text-564'];
+        
+            $queryUrl = 'https://fincom.bitrix24.ru/rest/114/keviule2jvconggu/crm.lead.add.json';
+
+            $queryData = http_build_query(array(
+                'fields' => array(
+                    'TITLE' => 'Лид со страницы ' . $title,
+                    'NAME' => $name_861,
+                    'PHONE' => Array(
+                        "n0" => Array(
+                            "VALUE" => $phone_831,
+                            "VALUE_TYPE" => "WORK"
+                        )
+                    ),
+                    'EMAIL' => Array(
+                        "n0" => Array(
+                            "VALUE" => $email_141,
                             "VALUE_TYPE" => "WORK"
                         )
                     )
