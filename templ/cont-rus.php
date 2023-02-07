7d4c25807643ee8253cd31b1871bd18b9060444f
flashgrinder <flashgrinder92@gmail.com>
2023-02-07 11:50:58 +0400

    some fix

--- templ/cont-rus.php
+++ templ/cont-rus.php
@@ -1,5 +1,5 @@
 <div class="header-cont__country">
   <a class="header-cont__country-link" href="http://fc-g.com/contacti/#usa">США</a>
-  <a class="header-cont__country-link" href="http://fc-g.com/contacti/#israel">Израиль</a>
+  <!-- <a class="header-cont__country-link" href="http://fc-g.com/contacti/#israel">Израиль</a> -->
   <a class="header-cont__country-link" href="http://fc-g.com/contacti/#russia">Россия</a>
 </div>
